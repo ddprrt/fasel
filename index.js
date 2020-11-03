c562163e9028f386a5ab219294d8e2520af9977c
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-11-03 14:18:54 +0100

    more responsive

--- index.js
+++ index.js
@@ -8,24 +8,34 @@ const folder = args[1];
 
 const funcs = {};
 
-fastify.post("/:func", function (request, response) {
-  if (
-    typeof request.params === "object" &&
-    typeof request.params.hasOwnProperty("func")
-  ) {
-    //@ts-ignore
-    const { func } = request.params;
-    if (!funcs[func]) {
-      funcs[func] = require(path.resolve(`${folder}/${func}`));
-    }
-    return funcs[func](request, response);
-  }
-  //@ts-ignore
-  response.send({ ...request.params, ...request.body });
-});
+fastify.post("/:func", handler());
+fastify.get("/:func", handler());
 
 fastify.listen(port, "0.0.0.0", function (err) {
   if (err) {
     console.log(err);
   }
 });
+function handler() {
+  return function (request, response) {
+    if (
+      typeof request.params === "object" &&
+      typeof request.params.hasOwnProperty("func")
+    ) {
+      //@ts-ignore
+      const { func } = request.params;
+      if (!funcs[func]) {
+        try {
+          funcs[func] = require(path.resolve(`${folder}/${func}`));
+        } catch (e) {
+          response.status(404).send({
+            msg: "function not found",
+          });
+        }
+      }
+      return funcs[func](request, response);
+    }
+    //@ts-ignore
+    response.send({ ...request.params, ...request.body });
+  };
+}
