a3f2f21a87540e410cf6a86b53ea8c69ceda0abe
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-11-03 14:23:11 +0100

    <3 typescript

--- index.js
+++ index.js
@@ -8,34 +8,42 @@ const folder = args[1];
 
 const funcs = {};
 
-fastify.post("/:func", handler());
-fastify.get("/:func", handler());
+fastify.post("/:func", handler);
+fastify.get("/:func", handler);
+
+fastify.post("what", (req, res) => {});
 
 fastify.listen(port, "0.0.0.0", function (err) {
   if (err) {
     console.log(err);
   }
 });
-function handler() {
-  return function (request, response) {
-    if (
-      typeof request.params === "object" &&
-      typeof request.params.hasOwnProperty("func")
-    ) {
-      //@ts-ignore
-      const { func } = request.params;
-      if (!funcs[func]) {
-        try {
-          funcs[func] = require(path.resolve(`${folder}/${func}`));
-        } catch (e) {
-          response.status(404).send({
-            msg: "function not found",
-          });
-        }
-      }
-      return funcs[func](request, response);
-    }
+
+/** @typedef {import("fastify").FastifyRequest} FastifyRequest */
+/** @typedef {import("fastify").FastifyReply} FastifyReply */
+
+/**
+ * @param {FastifyRequest} request
+ * @param {FastifyReply} response
+ */
+function handler(request, response) {
+  if (
+    typeof request.params === "object" &&
+    typeof request.params.hasOwnProperty("func")
+  ) {
     //@ts-ignore
-    response.send({ ...request.params, ...request.body });
-  };
+    const { func } = request.params;
+    if (!funcs[func]) {
+      try {
+        funcs[func] = require(path.resolve(`${folder}/${func}`));
+      } catch (e) {
+        response.status(404).send({
+          msg: "function not found",
+        });
+      }
+    }
+    return funcs[func](request, response);
+  }
+  //@ts-ignore
+  response.send({ ...request.params, ...request.body });
 }
