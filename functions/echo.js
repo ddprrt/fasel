5ee3311019e0a129b06a1b5e89f9b54918d46fb6
Baumgartner <Stefan.Baumgartner@dynatrace.com>
2020-11-10 13:47:14 +0100

    asnyc for fun(cs)

--- functions/echo.js
+++ functions/echo.js
@@ -3,7 +3,7 @@
  * @param {import("fastify").FastifyRequest} request
  * @param {import("fastify").FastifyReply} reply
  */
-module.exports = function (request, reply) {
+module.exports = async function (request, reply) {
   const body = request.body || request.query;
   if (body) {
     return reply.send({ _msg: "Hello from Echo", body });
